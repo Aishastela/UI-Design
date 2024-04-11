844c44b3cf9414129ff98cd094da04ce99391185
Aishastela <101183583+Aishastela@users.noreply.github.com>
2024-04-11 21:09:01 +0630

    change into separate file

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// function search() {
+//     var input = document.getElementById('searchInput').value;
+//     // Perform search functionality here based on the input value
+//     console.log('Searching for:', input);
+//     // You can modify this function to perform search operations as needed
+// }
+
+$(document).ready(function() {
+    $("#search").click(function() {
+        var searchText = $("#searchInput").val();
+        console.log(searchText);
+    });
+});
